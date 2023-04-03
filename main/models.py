9f351c6cf6c1f1947feed35f1b76593b2b73f7c4
LuciofsMelo <lucius.mat@gmail.com>
2023-04-02 21:12:32 -0300

    Criados a tabela livros e o admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Livros(models.Model):
+
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=40)
+    editora = models.CharField(max_length=30)
+    data_cadastro = models.DateField()
+    edicao = models.CharField(max_length=30)
+    ano_edicao = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    emprestado = models.BooleanField(default=False)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Livro'
+
+
+def __str__(self):
+    return self.titulo
+
