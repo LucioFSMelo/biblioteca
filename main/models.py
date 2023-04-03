7a3a47f9beb7754449a34a14b40e52d118117d6f
LuciofsMelo <lucius.mat@gmail.com>
2023-04-02 21:41:26 -0300

    Modificando o campo data_cadastro do DB

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from datetime import date
 
 class Livros(models.Model):
 
     titulo = models.CharField(max_length=100)
     autor = models.CharField(max_length=40)
     editora = models.CharField(max_length=30)
-    data_cadastro = models.DateField()
+    data_cadastro = models.DateField(default= date.today)
     edicao = models.CharField(max_length=30)
     ano_edicao = models.CharField(max_length=50)
     categoria = models.CharField(max_length=50)
@@ -14,8 +15,7 @@ class Livros(models.Model):
 
     class Meta:
         verbose_name = 'Livro'
-
-
-def __str__(self):
-    return self.titulo
+        
+    def __str__(self):
+        return self.titulo
 
