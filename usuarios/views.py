a52b1a5e3c4d2338ce2d4d79d5a3e57851c9a7bb
LuciofsMelo <lucius.mat@gmail.com>
2023-04-05 12:22:51 -0300

    Criados o model cadastro e a página de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
+from django.shortcuts import redirect
+from hashlib import sha256
 
 def login(requst):
     return HttpResponse('login')
@@ -7,4 +10,30 @@ def login(requst):
 
 def cadastro(request):
     return render(request, 'cadastro.html')
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    senha = request.POST.get('senha')
+    email = request.POST.get('email')
+
+    usuario = Usuario.objects.filter(email = email)
+
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=1')
+    
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=2')
+    
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+    
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome, 
+                          email = email, 
+                          senha = senha)
+        usuario.save()
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
 
