58be7171dd07fa674df1d1d2b164805c30d83be1
LuciofsMelo <lucius.mat@gmail.com>
2023-04-09 20:46:00 -0300

    Implementando a busca do usuário na home.

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from usuarios.models import Usuario
 
 def home(request):
     if request.session.get('usuario'): #Ao tentar pegar uma session chamada usuário, se ela existir
-        return HttpResponse('Hello') # Devemos mostrar o sistema
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome # Buscando o único usuário que atende as condições.
+        return HttpResponse(f'Olá {usuario}') # Devemos mostrar o sistema
     else:
         return redirect('/auth/login/?status=2') #Se não, redireciona para a página de login
