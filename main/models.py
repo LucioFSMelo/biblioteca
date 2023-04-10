ca5ddc613ad9623b3f68715c883a140d26d21f87
LuciofsMelo <lucius.mat@gmail.com>
2023-04-09 21:47:13 -0300

    Criada a tabela Categoria.

--- main/models.py
+++ main/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from datetime import date
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
+
 class Livros(models.Model):
 
     titulo = models.CharField(max_length=100)
@@ -9,9 +16,10 @@ class Livros(models.Model):
     data_cadastro = models.DateField(default= date.today)
     edicao = models.CharField(max_length=30)
     ano_edicao = models.CharField(max_length=50)
-    categoria = models.CharField(max_length=50)
     emprestado = models.BooleanField(default=False)
     description = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete= models.DO_NOTHING)
+    
 
     class Meta:
         verbose_name = 'Livro'
